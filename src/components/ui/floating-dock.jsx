ee8a73ea309581aa5c545638a15fbe76ef23d0d5
Mishthi Garg <mishthigarg1306@gmail.com>
2026-06-27 00:26:40 +0530

    mobile floating dock

--- src/components/ui/floating-dock.jsx
+++ src/components/ui/floating-dock.jsx
@@ -55,7 +55,7 @@ const FloatingDockMobile = ({
                 <a
                   href={item.href}
                   key={item.title}
-                  className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 dark:bg-neutral-900">
+                  className="border border-yellow-700 flex h-10 w-10 items-center justify-center rounded-full bg-yellow-50 dark:bg-neutral-900">
                   <div className="h-4 w-4">{item.icon}</div>
                 </a>
               </motion.div>
@@ -65,7 +65,7 @@ const FloatingDockMobile = ({
       </AnimatePresence>
       <button
         onClick={() => setOpen(!open)}
-        className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 dark:bg-neutral-800">
+        className="border border-gray-500 flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 dark:bg-neutral-800">
         <IconLayoutNavbarCollapse className="h-5 w-5 text-neutral-500 dark:text-neutral-400" />
       </button>
     </div>
