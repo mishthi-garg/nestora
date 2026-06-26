0e8d5b38f64c9974afe64d450e4f3e81f8171292
Mishthi Garg <mishthigarg1306@gmail.com>
2026-06-27 00:33:46 +0530

    dark mode dock removed

--- src/components/ui/floating-dock.jsx
+++ src/components/ui/floating-dock.jsx
@@ -55,7 +55,7 @@ const FloatingDockMobile = ({
                 <a
                   href={item.href}
                   key={item.title}
-                  className="border border-yellow-700 flex h-10 w-10 items-center justify-center rounded-full bg-yellow-50 dark:bg-neutral-900">
+                  className="border border-yellow-700 flex h-10 w-10 items-center justify-center rounded-full bg-yellow-50">
                   <div className="h-4 w-4">{item.icon}</div>
                 </a>
               </motion.div>
@@ -65,8 +65,8 @@ const FloatingDockMobile = ({
       </AnimatePresence>
       <button
         onClick={() => setOpen(!open)}
-        className="border border-gray-500 flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 dark:bg-neutral-800">
-        <IconLayoutNavbarCollapse className="h-5 w-5 text-neutral-500 dark:text-neutral-400" />
+        className="border border-gray-500 flex h-10 w-10 items-center justify-center rounded-full bg-gray-50">
+        <IconLayoutNavbarCollapse className="h-5 w-5 text-neutral-500" />
       </button>
     </div>
   );
@@ -82,7 +82,7 @@ const FloatingDockDesktop = ({
       onMouseMove={(e) => mouseX.set(e.pageX)}
       onMouseLeave={() => mouseX.set(Infinity)}
       className={cn(
-        "mx-auto hidden h-16 items-end gap-4 rounded-4xl bg-[rgb(255,252,235)] px-8 pb-3 md:flex dark:bg-neutral-900",
+        "mx-auto hidden h-16 items-end gap-4 rounded-4xl bg-[rgb(255,252,235)] px-8 pb-3 md:flex",
         className
       )}>
       {items.map((item) => (
@@ -143,14 +143,14 @@ function IconContainer({
         style={{ width, height }}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
-        className="relative flex aspect-square items-center justify-center rounded-full bg-yellow-100 dark:bg-neutral-800">
+        className="relative flex aspect-square items-center justify-center rounded-full bg-yellow-100">
         <AnimatePresence>
           {hovered && (
             <motion.div
               initial={{ opacity: 0, y: 10, x: "-50%" }}
               animate={{ opacity: 1, y: 0, x: "-50%" }}
               exit={{ opacity: 0, y: 2, x: "-50%" }}
-              className="absolute -top-8 left-1/2 w-fit rounded-md border border-gray-200 bg-gray-100 px-2 py-0.5 text-xs whitespace-pre text-neutral-700 dark:border-neutral-900 dark:bg-neutral-800 dark:text-white">
+              className="absolute -top-8 left-1/2 w-fit rounded-md border border-gray-200 bg-gray-100 px-2 py-0.5 text-xs whitespace-pre text-neutral-700">
               {title}
             </motion.div>
           )}
